9031ab73319997ffb015603084e1e9febd161775
cjlogue <cjlogue10@gmail.com>
2020-11-15 14:51:43 -0500

    render simple white square

--- public/main.js
+++ public/main.js
@@ -1,5 +1,13 @@
 //we'll grab a reference to the canvas and retrieve its WebGL context
+
 const main = () => {
     const canvas = document.getElementById("canvas");
     const gl = canvas.getContext("webgl");
+    const shaderProgram = initializeShaderProgram(gl)
+    const parameters = getProgramParameters(gl, shaderProgram);
+    const buffers = initializeBuffers(gl)
+
+    drawScene(gl, parameters, buffers)
+
 }
+
