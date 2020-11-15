8f1b426bac757afd06226fdd32e9504ba38eaf8f
cjlogue <cjlogue10@gmail.com>
2020-11-15 13:18:16 -0500

    getting started

---
+++ public/main.js
@@ -0,0 +1,5 @@
+//we'll grab a reference to the canvas and retrieve its WebGL context
+const main = () => {
+    const canvas = document.getElementById("canvas");
+    const gl = canvas.getContext("webgl");
+}
