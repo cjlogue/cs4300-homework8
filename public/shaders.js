f7e24a5e1e916e69bfb93b54984d5ec4b8477e73
cjlogue <cjlogue10@gmail.com>
2020-11-15 14:59:49 -0500

    add colors per vertex

--- public/shaders.js
+++ public/shaders.js
@@ -32,12 +32,16 @@ const getProgramParameters = (gl, shaderProgram) => {
     return {
         program: shaderProgram,
         attribLocations: {
-            vertexPosition: gl.getAttribLocation(shaderProgram, 'aVertexPosition'),
+            vertexPosition: gl.getAttribLocation
+            (shaderProgram, 'aVertexPosition'),
+            vertexColor: gl.getAttribLocation
+            (shaderProgram, 'aVertexColor'),
         },
         uniformLocations: {
-            projectionMatrix: gl.getUniformLocation(shaderProgram, 'uProjectionMatrix'),
-            modelViewMatrix: gl.getUniformLocation(shaderProgram, 'uModelViewMatrix'),
+            projectionMatrix: gl.getUniformLocation
+            (shaderProgram, 'uProjectionMatrix'),
+            modelViewMatrix: gl.getUniformLocation
+            (shaderProgram, 'uModelViewMatrix'),
         },
     };
 }
-
